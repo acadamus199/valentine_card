1861b94258f5145eeb783b77cdcbceb9d32d8008
adcBAKA <32962457+acadamus199@users.noreply.github.com>
2024-02-11 20:35:03 +0100

    Update btn_jump.js

--- source/btn_jump.js
+++ source/btn_jump.js
@@ -50,7 +50,7 @@ function acceptBtnFunction() {
     }, 1000);
     btn_accept.style.visibility = "hidden";
     btn.style.visibility = "hidden";
-    var lvU = setInterval(() => { alert("I love you <3"); clearInterval(lvU); }, 4000);
+    var lvU = setInterval(() => { alert("I love you <3"); clearInterval(lvU); window.close();}, 4000);
 }
 
 //Yes button click action
